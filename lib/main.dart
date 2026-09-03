5f9ca2a27ae69513b1efc5536b4ac5075d7a0f6b
Raza Rehman <raza.rehman1997@gmail.com>
2026-09-03 13:09:12 +0500

    Refactor MyApp to use StatelessWidget and add navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:untitled18/example_three.dart';
 import 'home_screen.dart';
 import 'example_two.dart';
+import 'example_three.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MainNavigation(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({Key? key}) : super(key: key);
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _currentIndex = 0;
+
+  final _screens = const [
+    HomeScreen(),
+    ExampleTwo(),
+    ExampleThree(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ExampleThree(),
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.article), label: 'Posts'),
+          BottomNavigationBarItem(icon: Icon(Icons.photo), label: 'Photos'),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Users'),
+        ],
+      ),
     );
   }
 }
